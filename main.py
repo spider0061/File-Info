ef92c84d1c92a585eadb4f22c45f60e703675cd1
b18030 <tushar22.tg.tg@gmail.com>
2020-10-22 17:00:54 +0530

    add Backbone Code

---
+++ main.py
@@ -0,0 +1,24 @@
+#Import the Required Libraries
+from tkinter import *
+from tkinter.filedialog import askopenfile 
+import matplotlib.pyplot as plt
+from PIL import Image, ImageTk
+from nltk import tokenize
+
+
+
+
+
+def open_file(arg):
+# Will be Done By B18010
+
+
+def open_keyword_file():
+#Will be Done By B18161
+
+
+
+def close_window(root): 
+    root.destroy()
+
+    
